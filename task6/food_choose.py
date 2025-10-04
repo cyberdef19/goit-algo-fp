bd4ea0c371bf2e1c32f7612d848635941487d080
Олексій Ієвенко <igrok1908@gmail.com>
2025-10-04 23:07:51 +0300

    Додано жадібний алгоритм максимізації співвідношення калорійності та вартості їжі

--- task6/food_choose.py
+++ task6/food_choose.py
@@ -1,3 +1,5 @@
+import heapq
+
 items = {
     "pizza": {"cost": 50, "calories": 300},
     "hamburger": {"cost": 40, "calories": 250},
@@ -7,10 +9,27 @@ items = {
     "potato": {"cost": 25, "calories": 350}
 }
 
-def greedy_alghorithm(budget: int) -> {}:
-    pass
+
+def greedy_algorithm(budget: float) -> {}:
+    ratio_calories_costs = [(-v["calories"]/v["cost"], k) for k, v in items.items()]
+    heapq.heapify(ratio_calories_costs)
+    result = {}
+    while budget > 0 and len(ratio_calories_costs) > 0:
+        val = heapq.heappop(ratio_calories_costs)
+        food = val[1]
+        item = items[food]
+        amount_food = budget//item["cost"]
+        if amount_food != 0:
+            result[food] = amount_food
+        amount_coins = amount_food * item["cost"]
+        budget -= amount_coins
+    return result
+
 
 
 def dynamic_programming(budget: int) -> {}:
     pass
+
+
+print(greedy_algorithm(4000))
 
