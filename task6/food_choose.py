59ae498ad6694e55ffbbe9a78e83a1f721649e22
Олексій Ієвенко <igrok1908@gmail.com>
2025-10-04 20:33:07 +0300

    Додано ащщв

---
+++ task6/food_choose.py
@@ -0,0 +1,16 @@
+items = {
+    "pizza": {"cost": 50, "calories": 300},
+    "hamburger": {"cost": 40, "calories": 250},
+    "hot-dog": {"cost": 30, "calories": 200},
+    "pepsi": {"cost": 10, "calories": 100},
+    "cola": {"cost": 15, "calories": 220},
+    "potato": {"cost": 25, "calories": 350}
+}
+
+def greedy_alghorithm(budget: int) -> {}:
+    pass
+
+
+def dynamic_programming(budget: int) -> {}:
+    pass
+
